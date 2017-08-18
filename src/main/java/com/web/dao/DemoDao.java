7ca518ed0613133347860c267a348ef150c12273
sukeyYang <ymyangshukai@126.com>
2017-08-18 17:43:32 +0800

    完成配置

--- src/main/java/com/web/dao/DemoDao.java
+++ src/main/java/com/web/dao/DemoDao.java
@@ -13,14 +13,13 @@ import org.hibernate.SessionFactory;
 import org.hibernate.transform.Transformers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 /**
  * Created by gaoyang on 16/2/29.
  */
 @Repository
 public class DemoDao extends BaseDao{
-
-    @Autowired
-    private SessionFactory sessionFactory;
 
     /**
      * 查询
