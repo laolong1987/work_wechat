af72b3a8465e2d2daeadfbe880432e6584a47f10
laolong1987 <232886019@qq.com>
2017-10-12 00:52:20 +0800

    增加worservice

---
+++ src/main/java/com/web/service/WorkFromService.java
@@ -0,0 +1,21 @@
+package com.web.service;
+
+import com.web.dao.DemoDao;
+import com.web.entity.Demo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+
+/**
+ * Created by gaoyang on 16/2/29.
+ */
+@Service("workFormService")
+public class WorkFromService {
+
+    public String getleaveform(){
+
+    }
+}
