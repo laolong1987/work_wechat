7091704ec5c9b4d15174e82ae9feb90801b1fa83
laolong1987 <232886019@qq.com>
2017-10-14 19:22:20 +0800

    添加 客饭申请单

--- src/main/java/com/web/controller/WorkController.java
+++ src/main/java/com/web/controller/WorkController.java
@@ -52,16 +52,19 @@ public class WorkController {
         String date2=ConvertUtil.safeToString(request.getParameter("date2"),"");
         String desc=ConvertUtil.safeToString(request.getParameter("desc"),"");
 
-        Employee employee= orgService.findEmployee("22000345");
+//        Employee employee= orgService.findEmployee("220238");
         JSONObject data=new JSONObject();
-        data.put("Depart",employee.getZzdwmc());
+//        data.put("Depart",employee.getZzdwmc());
+        data.put("Depart","总经理工作部");
 //        data.put("Position","普通职员");
         data.put("QjDays",day+".0");
         data.put("QjEnd",date2);
         data.put("QjStart",date1);
         data.put("QjType",type);
         data.put("Qjly",desc);
-        data.put("Ygxm",employee.getYgxm().replace(" ",""));
+//        data.put("Ygxm",employee.getYgxm().replace(" ",""));
+        data.put("Ygxm","华安");
+        data.put("Writer","220238");
 
         JSONObject json=new JSONObject();
         json.put("FormType","349");
@@ -82,10 +85,56 @@ public class WorkController {
         return "/jsp/app/addcar";
     }
 
+
+
+
+
     @RequestMapping(value = "/addguestmeal", method = RequestMethod.GET)
     public String addguestmeal(HttpServletRequest request,HttpServletResponse response) {
 
         return "/jsp/app/addguestmeal";
     }
 
+
+
+    @RequestMapping(value = "/createguestmeal", method = RequestMethod.POST)
+    public String createguestmeal(HttpServletRequest request,HttpServletResponse response) {
+        String lfdwjry=ConvertUtil.safeToString(request.getParameter("lfdwjry"),"");
+        String ycrq=ConvertUtil.safeToString(request.getParameter("ycrq"),"");
+        String yctype=ConvertUtil.safeToString(request.getParameter("yctype"),"");
+        String yczl=ConvertUtil.safeToString(request.getParameter("yczl"),"");
+        String ycsl=ConvertUtil.safeToString(request.getParameter("ycsl"),"");
+        String ycremark=ConvertUtil.safeToString(request.getParameter("ycremark"),"");
+        String ycbz=ConvertUtil.safeToString(request.getParameter("ycbz"),"");
+        String ycdd=ConvertUtil.safeToString(request.getParameter("ycdd"),"");
+
+
+//        Employee employee= orgService.findEmployee("220238");
+        JSONObject data=new JSONObject();
+//        data.put("Depart",employee.getZzdwmc());
+//        data.put("Position","普通职员");
+        data.put("Depart","总经理工作部");
+        data.put("lfdwjry",lfdwjry);
+        data.put("ycrq",ycrq);
+        data.put("yctype",yctype);
+        data.put("yczl",yczl);
+        data.put("ycsl",ycsl);
+        data.put("ycremark",ycremark);
+        data.put("ycbz",ycbz);
+        data.put("ycdd",ycdd);
+//        data.put("Ygxm",employee.getYgxm().replace(" ",""));
+
+        data.put("Ygxm","华安");
+        data.put("Writer","220238");
+        JSONObject json=new JSONObject();
+        json.put("FormType","323");
+        json.put("data",data);
+
+
+        String FormConfigID= workFromService.CreateFormInstance(json.toJSONString());
+        JSONObject resultJson = JSON.parseObject(FormConfigID);
+        String result= workFromService.StartFormWorkflow(resultJson.getString("FormConfigID"));
+        System.out.println(result);
+        return "/jsp/app/addguestmeal";
+    }
 }
